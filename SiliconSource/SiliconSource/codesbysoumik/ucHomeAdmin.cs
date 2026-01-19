33c766a068dfc766ec0a7b08fcecdac3a5dcd0b1
M. A. Shahriar Rahman Soumik <48492294+codesbysoumik@users.noreply.github.com>
2026-01-19 23:38:56 +0600

    Admin Home

---
+++ SiliconSource/SiliconSource/codesbysoumik/ucHomeAdmin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SiliconSource
+{
+    public partial class ucHomeAdmin : UserControl
+    {
+        private AdminDashboard AdminDashboardForm { get; set; }
+
+        public ucHomeAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (AdminDashboardForm is AdminDashboard dashboard)
+            {
+                dashboard.RefreshHomeTab();
+            }
+        }
+    }
+}
