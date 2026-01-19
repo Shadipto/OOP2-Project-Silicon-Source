b89dceab0ae369b12fc67bc383205254f505539a
M. A. Shahriar Rahman Soumik <48492294+codesbysoumik@users.noreply.github.com>
2026-01-19 23:33:28 +0600

    Employee Password Update User Access Control

---
+++ SiliconSource/SiliconSource/codesbysoumik/PasswordUpdate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SiliconSource.Admin
+{
+    public partial class PasswordUpdate : Form
+    {
+        private string UserID { get; set; }
+        private DataAccess Da { get; set; }
+
+        private Form UpdateEmployeeForm { get; set; }
+
+        public PasswordUpdate(Form updateEmployeeForm, string userID)
+        {
+            InitializeComponent();
+            this.Da = new DataAccess();
+            this.UpdateEmployeeForm = updateEmployeeForm;
+            this.UserID = userID;
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            string newPassword = ucNewPassword.TextboxText;
+            string confirmPassword = ucConfirmPassword.TextboxText;
+
+            if (string.IsNullOrEmpty(newPassword) ) 
+            {
+                MessageBox.Show("Fields cannot be empty.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (newPassword != confirmPassword)
+            {
+                MessageBox.Show("Passwords do not match.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            else if (newPassword == confirmPassword)
+            {
+                try
+                {
+                    string passwordHash = PasswordHasher.GenerateSHA256Hash(newPassword);
+                    string updateQuery = $"UPDATE AppUser SET PasswordHash = '{passwordHash}' WHERE UserID = '{UserID}';";
+
+                    int rowsAffected = Da.ExecuteDMLQuery(updateQuery);
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Password updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close();
+                        UpdateEmployeeForm.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to update password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred while updating the password:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            UpdateEmployeeForm.Show();
+        }
+    }
+}
